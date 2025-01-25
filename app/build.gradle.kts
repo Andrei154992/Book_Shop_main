06f12cc30e5a956f947c8406bef35ff9c2488f65
Andrei <andrei21.klei@gmail.com>
2025-01-26 02:40:18 +0400

    Третий коммит

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+
+}
+
+android {
+    namespace = "com.example.myapplication_1"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myapplication_1"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-database:21.0.0")
+    implementation ("com.google.firebase:firebase-storage:17.0.0")
+    implementation ("com.google.firebase:firebase-auth:21.1.0")
+    implementation ("com.github.rey5137:material:1.3.1")
+    implementation ("androidx.appcompat:appcompat:1.3.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.recyclerview:recyclerview:1.2.0")
+    implementation ("io.github.pilgr:paperdb:2.7.2")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.1")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+}
+
+
